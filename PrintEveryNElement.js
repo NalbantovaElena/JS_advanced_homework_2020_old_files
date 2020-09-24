39473ce203b883a816006915f950c0cb739a223e
NalbantovaElena <elena.nalbantova@mail.bg>
2020-09-24 09:50:50 +0300

    add new PrintEveryNElement.js

---
+++ PrintEveryNElement.js
@@ -0,0 +1,18 @@
+function solve(input) {
+
+    let step = Number(input.pop());
+    for (let i = 0; i < input.length; i += step) {
+        console.log(input[i]);
+    }
+}
+
+solve(
+    ['dsa',
+        'asd',
+        'test',
+        'tset',
+        '2']
+
+
+
+)
