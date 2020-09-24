d5a11e4da27f670a273deb02af1bb64ac28bd6e7
NalbantovaElena <elena.nalbantova@mail.bg>
2020-09-24 09:36:33 +0300

    add new file PrintAnArray.js

---
+++ PrintAnArray.js
@@ -0,0 +1,16 @@
+function solve(input=[]) {
+    let lastElement = input.pop();
+    //console.log(lastElement);
+    console.log(input.join(lastElement));
+
+}
+
+solve(
+    ['One',
+        'Two',
+        'Three',
+        'Four',
+        'Five',
+        '-']
+
+)
