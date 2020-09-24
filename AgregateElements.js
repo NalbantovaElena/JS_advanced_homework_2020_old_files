d2de9b46ee0108f7ab6034feffbbbdd703a56e3e
NalbantovaElena <elena.nalbantova@mail.bg>
2020-09-24 09:03:33 +0300

    add new file AgregateElements.js

---
+++ AgregateElements.js
@@ -0,0 +1,17 @@
+function solve(input) {
+    let sum = 0;
+    let sum2 = 0;
+   
+    for (let element of input) {
+        sum += element;
+        sum2 += 1 / element;
+       
+    }
+    console.log(sum);
+    console.log(sum2);
+    console.log(input.join(''));
+}
+
+solve(
+    [2, 4, 8, 16]
+)
